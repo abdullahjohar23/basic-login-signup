48b18fe88214df7f45c0d65aea10f4673fd9850c
Abdullah Johar <jrrabdullah23@gmail.com>
2025-07-22 12:47:54 +0600

    Scaffold added

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:basic_login_signup/home.dart';
-import 'package:basic_login_signup/phonehome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:basic_login_signup/phonehome.dart';
 class Wrapper extends StatefulWidget {
     const Wrapper({super.key});
 
@@ -12,19 +12,21 @@ class Wrapper extends StatefulWidget {
 class _WrapperState extends State<Wrapper> {
     @override
     Widget build(BuildContext context) {
-        return StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                    // if (snapshot.data!.emailVerified) {
-                        return HomePage();
-                    // } else {
-                    //     return Verifymail();
-                    // }
-                } else {
-                    return PhoneHome();
+        return Scaffold(
+            body: StreamBuilder(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                        // if (snapshot.data!.emailVerified) {
+                            return HomePage();
+                        // } else {
+                        //     return Verifymail();
+                        // }
+                    } else {
+                        return PhoneHome();
+                    }
                 }
-            }
+            )
         );
     }
 }
