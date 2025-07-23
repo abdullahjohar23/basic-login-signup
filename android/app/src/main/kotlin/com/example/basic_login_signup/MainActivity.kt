3713bd20577b0fbda2a56325d7323cd9dfe2e300
Abdullah Johar <jrrabdullah23@gmail.com>
2025-07-23 12:03:00 +0600

    FlutterActivity to FlutterFragmentActivity

--- android/app/src/main/kotlin/com/example/basic_login_signup/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/basic_login_signup/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.basic_login_signup
 
-import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+// class MainActivity : FlutterActivity() {
+// 
+// }
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+    
+}
