9101a75085a34c25922e9d0fe03380c0c29274c3
Abdullah Johar <jrrabdullah23@gmail.com>
2025-07-23 19:45:33 +0600

    page created & basic design done

---
+++ lib/fingerauth.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class FingerAuth extends StatefulWidget {
+    const FingerAuth({super.key});
+
+    @override
+    State<FingerAuth> createState() => _FingerAuthState();
+}
+
+class _FingerAuthState extends State<FingerAuth> {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                    SizedBox(height: 50),
+
+                    Center(
+                        child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Lora',
+                            ),
+                        ),
+                    ),
+
+                    SizedBox(height: 50),
+
+                    Icon(Icons.fingerprint_outlined, size: 75, color: Color.fromRGBO(117, 201, 253, 1)),
+
+                    SizedBox(height: 30),
+
+                    Center(
+                        child: Text(
+                            'Fingerprint Authentication',
+                            style: TextStyle(
+                                color: Color.fromRGBO(105, 136, 187, 1),
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Lora',
+                            ),
+                        ),
+                    ),
+
+                    Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 28, vertical: 5),
+
+                        child: Center(
+                            child: Text(
+                                'Authentication using fingerprint to proceed in application',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 18,
+                                ),
+                            ),
+                        ),
+                    ),
+
+                    SizedBox(height: 100),
+
+                    ElevatedButton(
+                        onPressed: () {},
+
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(117, 201, 253, 1),
+                        ),
+                        
+                        child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Text(
+                                'Authenticate',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                ),
+                            ),
+                        ),
+                    ),
+                ],
+            ),
+        );
+    }
+}
